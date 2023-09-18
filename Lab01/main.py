e7e7eaae9a152b9b5dd488c1481b706cbfd02325
claudiostn <claudiostn26@gmail.com>
2023-09-18 14:34:22 -0300

    Melhorar o teste da classe Estudante

--- Lab01/main.py
+++ Lab01/main.py
@@ -1,11 +1,21 @@
 from estudante import Estudante
 
 def main():
-    estudante = Estudante("Kenny", "69")
+    estudante1 = Estudante("Kenny", "69")
+    estudante2 = Estudante("Stan", "20")
+    estudante3 = Estudante("Cartman", "100")
+    estudante4 = Estudante("Kyle", "11")
 
-    estudante.adicionar_creditos(10)
+    lista_estudantes = [estudante1, estudante2, estudante3, estudante4]
 
-    print(f"Nome: {estudante.get_nome()}\nMatrícula: {estudante.get_matricula()}\nCréditos: {estudante.get_creditos()}")
+    nome = input("Nome do estudante: ")
+    creditos = int(input("Número de créditos: "))
+
+    for estudante in lista_estudantes:
+        if nome == estudante.get_nome():
+            estudante.adicionar_creditos(creditos)
+        print(f"\nNome: {estudante.get_nome()}\nMatrícula: {estudante.get_matricula()}\nCréditos: {estudante.get_creditos()}")
+
 
 if __name__ == "__main__":
     main()
