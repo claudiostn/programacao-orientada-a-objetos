9e2b62bb2b7478d65d0efc0a0c3e84f18ed65db3
claudiostn <claudiostn26@gmail.com>
2023-09-18 14:23:02 -0300

    Resolver erro no print

--- Lab01/main.py
+++ Lab01/main.py
@@ -1,8 +1,11 @@
 from estudante import Estudante
 
 def main():
-	estudante = Estudante("Kenny", "69")
+    estudante = Estudante("Kenny", "69")
 
     estudante.adicionar_creditos(10)
 
-    print(f"Nome:{estudante.get_nome()}\nMatrícula{estudante.get_matricula}\nCréditos:{estudante.get_creditos}")
+    print(f"Nome: {estudante.get_nome()}\nMatrícula: {estudante.get_matricula()}\nCréditos: {estudante.get_creditos()}")
+
+if __name__ == "__main__":
+    main()
