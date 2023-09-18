37d3463f90295a7d0d0141e27e9822b4b95fe350
claudiostn <claudiostn26@gmail.com>
2023-09-18 15:45:50 -0300

    Implementar os segundos no ClockDisplay

--- lab02/clockDisplay.py
+++ lab02/clockDisplay.py
@@ -4,26 +4,31 @@ class ClockDisplay:
     def __init__(self):
         self.__hours = NumberDisplay(24)
         self.__minutes = NumberDisplay(60)
+        self.__seconds = NumberDisplay(60)
         self.__display_string = ""
         self.__update_display()
     
     def get_time(self):
         return self.__display_string
 
-    def set_time(self, hour, minute):
+    def set_time(self, hour, minute, second):
         self.__hours.set_value(hour)
         self.__minutes.set_value(minute)
+        self.__seconds.set_value(second)
 
         self.__update_display()
 
     def time_tick(self):
-        self.__minutes.increment()
+        self.__seconds.increment()
         
-        if self.__minutes.get_value() == 0:
-            self.__hours.increment()
+        if self.__seconds.get_value() == 0:
+            self.__minutes.increment()
+            
+            if self.__minutes.get_value() == 0:
+                self.__hours.increment()
 
         self.__update_display()
 
     def __update_display(self):
-        self.__display_string = self.__hours.get_display_value() + ":" + self.__minutes.get_display_value()
+        self.__display_string = self.__hours.get_display_value() + ":" + self.__minutes.get_display_value() + ":" + self.__seconds.get_display_value()
     
--- lab02/main.py
+++ lab02/main.py
@@ -1,20 +1,14 @@
-from numberDisplay import NumberDisplay
 from clockDisplay import ClockDisplay
 
 def main():
-    display = NumberDisplay(24)
-    
-    display.set_value(19)
-    display.increment()
-    print(display.get_display_value())
-
     clock = ClockDisplay()
     print(clock.get_time())
-    for i in range(3620):
+    for i in range(60*6):
         clock.time_tick()
     print(clock.get_time())
 
-    clock.set_time(8, 20)
+    clock.set_time(8, 20, 59)
     print(clock.get_time())
+
 
 main()
