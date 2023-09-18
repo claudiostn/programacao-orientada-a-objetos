c477044b0aa2d0464435b1546196990629beb088
claudiostn <claudiostn26@gmail.com>
2023-09-18 15:07:33 -0300

    Definindo as horas e os minutos

--- lab02/clockDisplay.py
+++ lab02/clockDisplay.py
@@ -2,6 +2,6 @@ from numberDisplay import NumberDisplay
 
 class ClockDisplay:
     def __init__(self):
-        self.__hours = NumberDisplay()
-        self.__minutes = NumberDisplay()
-        
+        self.__hours = NumberDisplay(24)
+        self.__minutes = NumberDisplay(60)
+        self.__update_display()
