d8dae2504b7c1a8345f04eaeed03a31d7c4095ad
claudiostn <claudiostn26@gmail.com>
2023-09-18 14:40:21 -0300

    Renomear o arquivo

---
+++ Lab01/estudant.py
@@ -0,0 +1,26 @@
+class Estudante:
+    def __init__(self, nome, matricula):
+        self.__nome = nome
+        self.__matricula = matricula
+        self.__creditos = 0
+
+    def get_nome(self):
+        return self.__nome
+    
+    def get_matricula(self):
+        return self.__matricula
+
+    def get_creditos(self):
+        return self.__creditos
+    
+    def set_nome(self, novo_nome):
+        set_nome = novo_nome
+    
+    def set_matricula(self, nova_matricula):
+        self.__matricula = nova_matricula
+    
+    def set_creditos(self, valor):
+        self.__creditos = valor
+
+    def adicionar_creditos(self, valor):
+        self.__creditos += valor
