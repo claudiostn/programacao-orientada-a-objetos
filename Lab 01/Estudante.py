c3e1277f645c322a1779fedbbc5514367e2e2a08
claudiostn <claudiostn26@gmail.com>
2023-09-18 14:01:07 -0300

    Adicionar o Laboratório 1

---
+++ Lab 01/Estudante.py
@@ -0,0 +1,14 @@
+class Estudante:
+    def __init__(self, nome, matricula):
+        self.__nome = nome
+        self.__matricula = matricula
+        self.__creditos = 0
+
+    def get_nome():
+        return self.__nome
+    
+    def get_matricula(self):
+        return self.__matricula
+
+    def get_creditos(self):
+        return self.__creditos
