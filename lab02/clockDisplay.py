ca108822a6291e785425fc2902a65d15bae83e34
claudiostn <claudiostn26@gmail.com>
2023-09-18 15:09:42 -0300

    Definir o método de atualizar o display

--- lab02/clockDisplay.py
+++ lab02/clockDisplay.py
@@ -4,4 +4,8 @@ class ClockDisplay:
     def __init__(self):
         self.__hours = NumberDisplay(24)
         self.__minutes = NumberDisplay(60)
+
         self.__update_display()
+    
+    def __update_display(self):
+        self.__display_string = self.__hours.get_display_value() + ":" + self.__minutes.get_display_value()
