0989e73460027a2614284880ae9e1aca42e520d7
claudiostn <claudiostn26@gmail.com>
2023-09-18 15:24:14 -0300

    Definir o método que conta os minutos

--- lab02/clockDisplay.py
+++ lab02/clockDisplay.py
@@ -4,9 +4,26 @@ class ClockDisplay:
     def __init__(self):
         self.__hours = NumberDisplay(24)
         self.__minutes = NumberDisplay(60)
-
+        self.__display_string = ""
         self.__update_display()
     
+    def get_time(self):
+        return self.__display_string
+
+    def set_time(self, hour, minute):
+        self.__hours = NumberDisplay(hour)
+        self.__minutes = NumberDisplay(minute)
+
+        self.__update_display()
+
+    def time_tick(self):
+        self.__minutes.increment()
+        
+        if self.__minutes.get_display_value() == 0:
+            self.__hours.increment()
+
+        self.__update_display()
+        
     def __update_display(self):
         self.__display_string = self.__hours.get_display_value() + ":" + self.__minutes.get_display_value()
     
