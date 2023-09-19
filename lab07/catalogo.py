60c5b307bb686e0fdfd1f51ffb0c094f3c085360
claudiostn <claudiostn26@gmail.com>
2023-09-19 06:22:13 -0300

    Resolvendo problema de tabulação

--- lab07/catalogo.py
+++ lab07/catalogo.py
@@ -3,38 +3,37 @@ from dvd import DVD
 
 
 class Catalogo:
+    def __init__(self):
+        self.__lista = []
 
-  def __init__(self):
-    self.__lista = []
+    def inserir_item(self, item):
+        if not self.pesquisa_item(item):
+            self.__lista.append(item)
+            return True
+        else:
+            return False
 
-  def inserir_item(self, item):
-    if not self.pesquisa_item(item):
-      self.__lista.append(item)
-      return True
-    else:
-      return False
+    def pesquisa_item(self, item):
+        return item in self.__lista
 
-  def pesquisa_item(self, item):
-    return item in self.__lista
+    def listar(self):
+        cds = []
+        dvds = []
+        for item in self.__lista:
+            if isinstance(item, CD):
+                cds.append(item)
+            else:
+                dvds.append(item)
 
-  def listar(self):
-    cds = []
-    dvds = []
-    for item in self.__lista:
-      if isinstance(item, CD):
-        cds.append(item)
-      else:
-        dvds.append(item)
+        for cd in cds:
+            print(cd.get_titulo())
 
-    for cd in cds:
-      print(cd.get_titulo())
+        for dvd in dvds:
+            print(dvd.get_titulo())
 
-    for dvd in dvds:
-      print(dvd.get_titulo())
-
-  def remover_item(self, item):
-    if self.pesquisa_item(item):
-      self.__lista.remove(item)
-      return True
-    else:
-      return False
+    def remover_item(self, item):
+        if self.pesquisa_item(item):
+            self.__lista.remove(item)
+            return True
+        else:
+            return False
--- lab07/main.py
+++ lab07/main.py
@@ -4,26 +4,25 @@ from dvd import DVD
 
 
 def main():
+    catalogo = Catalogo()
 
-  catalogo = Catalogo()
+    cd1 = CD("CD 1", 120, False, "cd1", "a1", 12)
+    cd2 = CD("CD 2", 120, True, "cd2", "a2", 12)
+    cd3 = CD("CD 3", 120, False, "cd3", "a3", 12)
 
-  cd1 = CD("CD 1", 120, False, "cd1", "a1", 12)
-  cd2 = CD("CD 2", 120, True, "cd2", "a2", 12)
-  cd3 = CD("CD 3", 120, False, "cd3", "a3", 12)
+    dvd1 = DVD("DVD 1", 120, False, "dvd1", "d1")
+    dvd2 = DVD("DVD 2", 120, False, "dvd2", "d2")
+    dvd3 = DVD("DVD 3", 120, False, "dvd3", "d3")
 
-  dvd1 = DVD("DVD 1", 120, False, "dvd1", "d1")
-  dvd2 = DVD("DVD 2", 120, False, "dvd2", "d2")
-  dvd3 = DVD("DVD 3", 120, False, "dvd3", "d3")
+    catalogo.inserir_item(cd1)
+    catalogo.inserir_item(dvd1)
+    catalogo.inserir_item(cd2)
+    catalogo.inserir_item(dvd2)
+    catalogo.inserir_item(cd3)
+    catalogo.inserir_item(dvd3)
 
-  catalogo.inserir_item(cd1)
-  catalogo.inserir_item(dvd1)
-  catalogo.inserir_item(cd2)
-  catalogo.inserir_item(dvd2)
-  catalogo.inserir_item(cd3)
-  catalogo.inserir_item(dvd3)
-
-  catalogo.listar()
+    catalogo.listar()
 
 
 if __name__ == '__main__':
-  main()
+    main()
