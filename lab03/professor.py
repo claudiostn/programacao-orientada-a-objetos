96f405ea8250aeed77365438598723ab6899996b
claudiostn <claudiostn26@gmail.com>
2023-09-19 04:23:55 -0300

    Definir a classe Professor

---
+++ lab03/professor.py
@@ -0,0 +1,5 @@
+class Professor:
+    def __init__(self, nome, matricula, carga_horaria):
+        self.__nome = nome
+        self.__matricula = matricula
+        self.__carga_horaria = carga_horaria
