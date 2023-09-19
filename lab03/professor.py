217a51d8817626bb4694f9ef198d88e5d9a469a0
claudiostn <claudiostn26@gmail.com>
2023-09-19 04:47:44 -0300

    Modificar a classe Professor

--- lab03/professor.py
+++ lab03/professor.py
@@ -3,7 +3,9 @@ class Professor:
         self.__nome = nome
         self.__matricula = matricula
         self.__carga_horaria = carga_horaria
-
+        self.__carga_horaria_minima = 8
+        self.__carga_horaria_maxima = 20
+        
     def get_nome(self):
         return self.__nome
     
@@ -12,6 +14,12 @@ class Professor:
     
     def get_carga_horaria(self):
         return self.__carga_horaria
+    
+    def get_carga_minima(self):
+        return self.__carga_horaria_minima
+    
+    def get_carga_maxima(self):
+        return self.__carga_horaria_maxima
     
     def set_nome(self, nome):
         self.__nome = nome
@@ -23,7 +31,13 @@ class Professor:
         self.__carga_horaria = carga_horaria
 
     def mais_horas(self, valor):
-        self.__carga_horaria += valor
+        carga = self.__carga_horaria + valor
+
+        if self.__carga_horaria_minima <= carga <= self.__carga_horaria_maxima:
+            self.__carga_horaria = carga
 
     def menos_horas(self, valor):
-        self.__carga_horaria -= valor
+        carga = self.__carga_horaria - valor
+
+        if self.__carga_horaria_minima <= carga <= self.__carga_horaria_maxima:
+            self.__carga_horaria = carga
