1ced4bf1e1ce1fd5cb028565e457a4674eb132bb
claudiostn <claudiostn26@gmail.com>
2023-09-19 05:24:18 -0300

    Testar a classe

--- lab04/main.py
+++ lab04/main.py
@@ -3,14 +3,14 @@ from notebook import Notebook
 def main():
     notes = Notebook()
 
-    note = input()
-    notes.store_note(note)
-
-    note = input()
-    notes.store_note(note)
+    for i in range(10):
+        note = input()
+        notes.store_note(note)
 
     note = "Quack Quack"
     notes.store_note(note)
+
+    print(notes.has_notes())
 
     notes.show_note(1)
 
@@ -18,5 +18,10 @@ def main():
 
     notes.list_notes()
 
+    print(notes.compare_note("Quack Quack"))
+
+    notes.show_note_random()
+
+    notes.show_multi_note_random(5)
 
 main()
