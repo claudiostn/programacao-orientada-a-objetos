038ce128677bfa91e1893eb7e0ed1ec225305e99
claudiostn <claudiostn26@gmail.com>
2023-09-19 04:31:37 -0300

    Definir métodos para mudar a carga horaria

--- lab03/professor.py
+++ lab03/professor.py
@@ -13,11 +13,17 @@ class Professor:
     def get_carga_horaria(self):
         return self.__carga_horaria
     
-    def set_nome(self):
-        return self.__nome
+    def set_nome(self, nome):
+        self.__nome = nome
     
-    def set_matricula(self):
-        return self.__matricula
+    def set_matricula(self, matricula):
+        self.__matricula = matricula
     
-    def set_carga_horaria(self):
-        return self.__carga_horaria
+    def set_carga_horaria(self, carga_horaria):
+        self.__carga_horaria = carga_horaria
+
+    def mais_horas(self, valor):
+        self.__carga_horaria += valor
+
+    def menos_horas(self, valor):
+        self.__carga_horaria -= valor
