8f9a8492421585b191abb1a69c7eb28a0770cb4b
claudiostn <claudiostn26@gmail.com>
2023-09-19 04:28:44 -0300

    Definir os getters e setters

--- lab03/professor.py
+++ lab03/professor.py
@@ -3,3 +3,21 @@ class Professor:
         self.__nome = nome
         self.__matricula = matricula
         self.__carga_horaria = carga_horaria
+
+    def get_nome(self):
+        return self.__nome
+    
+    def get_matricula(self):
+        return self.__matricula
+    
+    def get_carga_horaria(self):
+        return self.__carga_horaria
+    
+    def set_nome(self):
+        return self.__nome
+    
+    def set_matricula(self):
+        return self.__matricula
+    
+    def set_carga_horaria(self):
+        return self.__carga_horaria
