f02096d628624123b8d861e09d7451f242be9dad
claudiostn <claudiostn26@gmail.com>
2023-09-19 05:35:59 -0300

    Definir os métodos que printam números aleatórios

--- lab05/randomTester.py
+++ lab05/randomTester.py
@@ -1,3 +1,14 @@
+import random
+
+
 class RandomTester:
     def __init__(self):
         pass
+
+    def print_one_random(self):
+        number = random.randint(1000)
+        print(number)
+
+    def print_multi_random(self, amount):
+        for i in range(amount):
+            self.print_one_random()
