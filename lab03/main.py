7adf5250ce692db907b1260ac97fb712c020b62b
claudiostn <claudiostn26@gmail.com>
2023-09-19 04:49:54 -0300

    Testar a classe Professor

--- lab03/main.py
+++ lab03/main.py
@@ -6,6 +6,9 @@ def main():
     horas = int(input())
     professor = Professor(nome, matricula, horas)
     
+    professor.mais_horas(10)
+    professor.menos_horas(5)
+
     print(f"Professor {professor.get_nome()} Matrícula {professor.get_matricula()} {professor.get_carga_horaria()} horas semanais")
 
 main()
