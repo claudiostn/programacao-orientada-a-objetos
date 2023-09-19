0dbb271f17aba4a51640a33d08c7d1ad23d6147b
claudiostn <claudiostn26@gmail.com>
2023-09-19 05:39:57 -0300

    Definir um método que joga um dado

--- lab05/randomTester.py
+++ lab05/randomTester.py
@@ -6,9 +6,11 @@ class RandomTester:
         pass
 
     def print_one_random(self):
-        number = random.randint(1000)
+        number = random.randint(0, 1000)
         print(number)
 
     def print_multi_random(self, amount):
         for i in range(amount):
             self.print_one_random()
+
+    def throw_dice(self):
