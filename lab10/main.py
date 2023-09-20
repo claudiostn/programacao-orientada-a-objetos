d2a935844d56ade33d876d74dd9a7b1f18a3fdea
claudiostn <claudiostn26@gmail.com>
2023-09-20 02:53:07 -0300

    Remover o exemplo

--- lab10/main.py
+++ lab10/main.py
@@ -1,15 +1,5 @@
 from tkinter import *
 
-class Application(Frame):
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
-        self.msg = Label(self, text="Hello, World!")
-        self.msg.pack()
-
-        self.bye = Button(self, text="Bye", command=self.quit)
-        self.bye.pack()
-        self.pack()
-
 root = Tk()
 conteudo = Frame(root)
 conteudo.pack()
