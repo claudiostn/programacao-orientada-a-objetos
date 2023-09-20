f34090c5e8b44cf5e243712fddb26a8f32bfcfa5
claudiostn <claudiostn26@gmail.com>
2023-09-20 02:55:52 -0300

    Configurar cor e texto do botão

--- lab10/main.py
+++ lab10/main.py
@@ -3,6 +3,8 @@ from tkinter import *
 root = Tk()
 conteudo = Frame(root)
 botao = Button(conteudo)
+botao.configure(text="Hello, World!")
+botao.configure(background="green")
 botao.pack()
 conteudo.pack()
 
