0584d26afbc49f05dd96bbb020de714ee7a299cc
claudiostn <claudiostn26@gmail.com>
2023-09-20 02:56:50 -0300

    Criar e configurar um rotulo

--- lab10/main.py
+++ lab10/main.py
@@ -2,9 +2,12 @@ from tkinter import *
 
 root = Tk()
 conteudo = Frame(root)
+rotulo = Label(conteudo)
+rotulo.configure(text="Eu sou um rótulo")
 botao = Button(conteudo)
 botao.configure(text="Hello, World!")
 botao.configure(background="green")
+rotulo.pack()
 botao.pack()
 conteudo.pack()
 
